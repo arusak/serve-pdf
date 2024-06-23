c9d903ac99c5df0e38b3fcb0a77a5a800c4e477d
Anton Rusak <anton.rusak@perfectart.com>
2024-06-23 17:14:26 +0300

    add --hostname arg

--- app.js
+++ app.js
@@ -3,24 +3,30 @@ import fs from 'fs'
 import https from 'https'
 // import cors from 'cors'
 import path from 'path'
+import minimist from 'minimist'
 
-const HOSTNAME = '192.168.133.182';
+// Default HOSTNAME if not provided via command line arguments
+const DEFAULT_HOSTNAME = 'localhost'
 const PORT = 8787
-const PDF_FILE_PATH = path.join(process.cwd(), 'document.pdf');
-const HTML_FILE_PATH = path.join(process.cwd(), 'index.html');
+const PDF_FILE_PATH = path.join(process.cwd(), 'document.pdf')
+const HTML_FILE_PATH = path.join(process.cwd(), 'index.html')
+
+// Parse command line arguments
+const args = minimist(process.argv.slice(2))
+const HOSTNAME = args.hostname || DEFAULT_HOSTNAME
 
 const app = express()
 app.use(express.json())
 // app.use(cors()) // Enable CORS for all routes
 
 app.get('/pdf', (req, res) => {
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'inline; filename="document.pdf"');
-    res.sendFile(PDF_FILE_PATH);
+    res.setHeader('Content-Type', 'application/pdf')
+    res.setHeader('Content-Disposition', 'inline; filename="document.pdf"')
+    res.sendFile(PDF_FILE_PATH)
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(HTML_FILE_PATH);
+    res.sendFile(HTML_FILE_PATH)
 });
 
 const sslOptions = {
